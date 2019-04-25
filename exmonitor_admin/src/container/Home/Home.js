af932943f4701df163580e44a8f1546edce3bd4b
lijiawei <lijiawei.0602@bytedance.com>
2019-04-25 23:53:22 +0800

    add home rate

--- exmonitor_admin/src/container/Home/Home.js
+++ exmonitor_admin/src/container/Home/Home.js
@@ -14,11 +14,17 @@ class Home extends React.Component {
         super(props);
         this.state = {
             tab: '',
+            monthChart: null,
+            dayChart: null,
         }
+        this.monthChart = React.createRef();
+        this.dayChart = React.createRef();
     }
 
     componentDidMount () {
-
+        window.onresize = function () {
+            this.state.monthChart.resize();
+        }.bind(this);
     }
 
     componentWillReceiveProps (nextProps) {
@@ -48,6 +54,9 @@ class Home extends React.Component {
                 const monthChart = echarts.init(document.getElementById('jsErrorMonthChart'));
                 const options = jsErrorMonthOpt(arr);
                 monthChart.setOption(options);
+                this.setState({
+                    monthChart,
+                })
             });
             if (this.state.tab === 'day') {
                 const obj = {
@@ -57,9 +66,16 @@ class Home extends React.Component {
                     const dayChart = echarts.init(document.getElementById('jsErrorDayChart'));
                     const options = jsErrorDayOpt(res);
                     dayChart.setOption(options);
+                    this.setState({
+                        dayChart,
+                    })
                 });
             }
         }
+    }
+
+    componentWillUnmount () {
+        window.onresize = null;
     }
 
     handleTabChange = (key) => {
@@ -76,6 +92,13 @@ class Home extends React.Component {
                     const dayChart = echarts.init(document.getElementById('jsErrorDayChart'));
                     const options = jsErrorDayOpt(res);
                     dayChart.setOption(options);
+                    this.setState({
+                        dayChart,
+                    }, () => {
+                        window.addEventListener('resize', () => {
+                            this.state.dayChart.resize();
+                        })
+                    })
                 });
             }
         } else {
@@ -91,34 +114,34 @@ class Home extends React.Component {
                 <Row style={{border: '1px solid #e8e8e8'}}>
                     <Col span={16}>
                         <Tabs defaultActiveKey="month" onChange={this.handleTabChange}>
-                            <TabPane tab="月统计" key="month">
-                                <div id="jsErrorMonthChart" className="jsErrorChart"></div>
+                            <TabPane tab={<span><Icon type="bar-chart" />月统计</span>} key="month">
+                                <div id="jsErrorMonthChart" ref={this.monthChart} className="jsErrorChart"></div>
                             </TabPane>
-                            <TabPane tab="天统计" key="day">
-                                <div id="jsErrorDayChart" className="jsErrorChart"></div>
+                            <TabPane tab={<span><Icon type="line-chart" />天统计</span>} key="day">
+                                <div id="jsErrorDayChart" ref={this.dayChart} className="jsErrorChart"></div>
                             </TabPane>
                         </Tabs>
                     </Col>
                     <Col span={8}>
                         <Tabs defaultActiveKey="rate" onChange={this.handleTabChange} className="rate">
-                            <TabPane tab="总错误" key="rate">
+                            <TabPane tab={<span><Icon type="heat-map" />错误概览</span>} key="rate" className="rate-tab">
                                 <div className="rate-item">
-                                    <Icon type="thunderbolt" theme="filled" />
+                                    <div style={{textAlign: 'center'}}><Icon type="thunderbolt" theme="filled" style={{fontSize: '40px',color: '#727082'}} /></div>
                                     <div className="rate-item-font">总错误率</div>
                                     <div className="rate-item-font">1</div>
                                 </div>
                                 <div className="rate-item">
-                                    <Icon type="windows" theme="filled" />
+                                    <div style={{textAlign: 'center'}}><Icon type="windows" theme="filled" style={{fontSize: '40px',color: '#727082'}} /></div>
                                     <div className="rate-item-font">PC错误率</div>
                                     <div className="rate-item-font">2</div>
                                 </div>
                                 <div className="rate-item">
-                                    <Icon type="android" theme="filled" />
+                                    <div style={{textAlign: 'center'}}><Icon type="android" theme="filled" style={{fontSize: '40px',color: '#727082'}} /></div>
                                     <div className="rate-item-font">Android错误率</div>
                                     <div className="rate-item-font">3</div>
                                 </div>
                                 <div className="rate-item">
-                                    <Icon type="apple" theme="filled" />
+                                    <div style={{textAlign: 'center'}}><Icon type="apple" theme="filled" style={{fontSize: '40px', color: '#727082'}} /></div>
                                     <div className="rate-item-font">iOS错误率</div>
                                     <div className="rate-item-font">3</div>
                                 </div>
